a044770027f6666edb92070111f3bc165affa758
Dominika <domi_nika@hotmail.co.uk>
2021-08-01 11:44:38 +0100

    added authentication lgoic (WIP)

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const express = require("express");
 const path = require("path");
 const db = require("./config/connection");
 const routes = require("./routes");
+const authenticate = require("./utils/auth2");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,16 +15,19 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-}
+// // if we're in production, serve client/build as static assets
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../client/build")));
+// }
 
 app.use(routes);
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  // context: ({ req }) => {
+  //   authenticate(req);
+  // },
 });
 
 db.once("open", () => {
