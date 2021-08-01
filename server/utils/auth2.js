0b625c4a4033ee894eeb828b4d053d9208198109
Dominika <domi_nika@hotmail.co.uk>
2021-08-01 15:48:58 +0100

    added logic for progressing with unauth requests

--- server/utils/auth2.js
+++ server/utils/auth2.js
@@ -2,24 +2,18 @@ const jwt = require("jsonwebtoken");
 const { AuthenticationError } = require("apollo-server");
 
 const authenticate = (req) => {
-  console.log("authenticating");
-  console.log("this is the request", req);
-  const secret = "mysecretsshhhhh";
-  const expiration = "2h";
-
-  let token = req.headers.authorization;
-
-  // ["Bearer", "<tokenvalue>"]
-  if (req.headers.authorization) {
-    token = token.split(" ").pop().trim();
-  }
-
-  if (!token) {
-    throw new AuthenticationError("user must be logged in");
-  }
-
-  // verify token and get user data out of it
   try {
+    const secret = "mysecretsshhhhh";
+    const expiration = "2h";
+
+    if (!req.headers.authorization) {
+      return;
+    }
+
+    let token = req.headers.authorization;
+    token = token.split(" ").pop().trim();
+
+    // verify token and get user data out of it
     const { data: user } = jwt.verify(token, secret, { maxAge: expiration });
     console.log("this is the user from auth token", user);
     return { user };
