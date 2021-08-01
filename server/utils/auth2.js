0b796ed0ff7913d210368f60bd28e808249946de
Dominika <domi_nika@hotmail.co.uk>
2021-08-01 19:48:50 +0100

    fixed auth to save in context

--- server/utils/auth2.js
+++ server/utils/auth2.js
@@ -1,22 +1,25 @@
 const jwt = require("jsonwebtoken");
-const { AuthenticationError } = require("apollo-server");
 
 const authenticate = (req) => {
   try {
     const secret = "mysecretsshhhhh";
     const expiration = "2h";
 
+    console.log(req.headers);
+
     if (!req.headers.authorization) {
+      console.log("no headers");
       return;
     }
 
     let token = req.headers.authorization;
     token = token.split(" ").pop().trim();
+    console.log(token);
 
     // verify token and get user data out of it
     const { data: user } = jwt.verify(token, secret, { maxAge: expiration });
     console.log("this is the user from auth token", user);
-    return { user };
+    return user;
   } catch {
     console.info(error);
     throw new ApolloError("Internal server error. Please try again soon");
