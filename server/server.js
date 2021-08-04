c2fb522b641c52c222d288939a502cd53b09f93a
Dominika <domi_nika@hotmail.co.uk>
2021-08-04 19:36:03 +0100

    deleted api routes and fns

--- server/server.js
+++ server/server.js
@@ -3,24 +3,8 @@ const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./schemas");
 const resolvers = require("./resolvers");
 
-const express = require("express");
-const path = require("path");
 const db = require("./config/connection");
-const routes = require("./routes");
-const authenticate = require("./utils/auth2");
-
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// // if we're in production, serve client/build as static assets
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../client/build")));
-// }
-
-app.use(routes);
+const authenticate = require("./utils/auth");
 
 const server = new ApolloServer({
   typeDefs,
@@ -32,7 +16,6 @@ const server = new ApolloServer({
 });
 
 db.once("open", () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
   server.listen().then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
   });
